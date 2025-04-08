354e90546df984739e2b6376c13bdc166454d519
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-09 00:56:09 +0100

    feat: integrate IconMenu component into Layout and display icons

--- front-end/src/components/IconMenu/IconMenu.jsx
+++ front-end/src/components/IconMenu/IconMenu.jsx
@@ -1,14 +1,20 @@
 import styles from "./IconMenu.module.css";
+import BAG from "../../assets/bag.svg";
+import HEART from "../../assets/heart.svg";
 import { Link } from "react-router-dom";
 
 export function IconMenu() {
   return (
     <ul className={styles}>
       <li>
-        <Link to="/favorite"></Link>
+        <Link to="/favorite">
+          <img src={BAG} />
+        </Link>
       </li>
       <li>
-        <Link to="/basket"></Link>
+        <Link to="/basket">
+          <img src={HEART} />
+        </Link>
       </li>
     </ul>
   );
