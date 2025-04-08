01b3c540f84c74e8974cc667232acfba059bcf9f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-09 00:28:40 +0100

    feat: style CurrencySelector component with CSS module

--- front-end/src/components/CurrencySelector/CurrencySelector.jsx
+++ front-end/src/components/CurrencySelector/CurrencySelector.jsx
@@ -3,16 +3,10 @@ import styles from "./CurrencySelector.module.css";
 
 export function CurrencySelector() {
   return (
-    <select>
-      <option value={CURRENCIES.GBP} className={styles.option}>
-        {CURRENCIES.GBP}
-      </option>
-      <option value={CURRENCIES.PLN} className={styles.option}>
-        {CURRENCIES.PLN}
-      </option>
-      <option value={CURRENCIES.USD} className={styles.option}>
-        {CURRENCIES.USD}
-      </option>
+    <select className={styles.currencySelector}>
+      <option value={CURRENCIES.GBP}>{CURRENCIES.GBP}</option>
+      <option value={CURRENCIES.PLN}>{CURRENCIES.PLN}</option>
+      <option value={CURRENCIES.USD}>{CURRENCIES.USD}</option>
     </select>
   );
 }
