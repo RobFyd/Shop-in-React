e197e8cad7d1f15dc9441594fca6af9c62aab3ea
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-14 23:52:42 +0100

    feat: apply styles to CategoryMenu component for improved layout

--- front-end/src/components/CategoryMenu/CategoryMenu.jsx
+++ front-end/src/components/CategoryMenu/CategoryMenu.jsx
@@ -1,9 +1,10 @@
 import { CATEGORIES } from "../../constants/categories";
 import { NavLink } from "react-router-dom";
+import styles from "./CategoryMenu.module.css";
 
 export function CategoryMenu() {
   return (
-    <div>
+    <div className={styles.categoryMenu}>
       <ul>
         {CATEGORIES.map((category) => {
           return (
