424539ef6f055473a0f23fd0433aedf3027d77b1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-16 01:34:49 +0100

    feat: add Hero component with promotional content

---
+++ front-end/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import { CeneteredContent } from "../CenteredContent/CenteredContent";
+
+export function Hero() {
+  return (
+    <div>
+      <CeneteredContent>
+        <div>
+          <h2>Summer promotions up to -70%</h2>
+          <p>Only the best deals!</p>
+          <button>Check the product</button>
+        </div>
+      </CeneteredContent>
+    </div>
+  );
+}
