717942779e544e9adc23c8f306995d3ae17a4d94
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-22 23:59:42 +0100

    feat: implement Bestsellers component to display top products

--- front-end/src/components/Bestsellers/Bestsellers.jsx
+++ front-end/src/components/Bestsellers/Bestsellers.jsx
@@ -1,1 +1,15 @@
-export function Bestsellers({ products }) {}
+import { CeneteredContent } from "../CenteredContent/CenteredContent";
+import { Product } from "../Product/Product";
+
+export function Bestsellers({ products }) {
+  return (
+    <CeneteredContent>
+      <h2>Check out our bestsellers</h2>
+      <div>
+        {products.map((product) => {
+          return <Product key={product.id} product={product} />;
+        })}
+      </div>
+    </CeneteredContent>
+  );
+}
