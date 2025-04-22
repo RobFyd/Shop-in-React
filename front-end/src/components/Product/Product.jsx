bab4e29f5b3a72a1a6eec0f7b00c9ee150dad99c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-23 00:34:04 +0100

    feat: add heart-full SVG asset and update Product component to display product details

--- front-end/src/components/Product/Product.jsx
+++ front-end/src/components/Product/Product.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function Product({ product }) {
-  return <p>{product.productName}</p>;
+  return (
+    <Link>
+      <img src={product.photos[0]} />
+      <h3>{product.productName}</h3>
+      <p>{product.priceGBP}</p>
+    </Link>
+  );
 }
