ad28c7c6bcb98329f38fa1131bf2361d4f9c3314
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-23 00:16:51 +0100

    feat: style Bestsellers component with CSS module for improved layout and presentation

--- front-end/src/components/Bestsellers/Bestsellers.jsx
+++ front-end/src/components/Bestsellers/Bestsellers.jsx
@@ -1,11 +1,12 @@
+import styles from "./Bestsellers.module.css";
 import { CeneteredContent } from "../CenteredContent/CenteredContent";
 import { Product } from "../Product/Product";
 
 export function Bestsellers({ products }) {
   return (
     <CeneteredContent>
-      <h2>Check out our bestsellers</h2>
-      <div>
+      <h2 className={styles.bestsellerHeader}>Check out our bestsellers</h2>
+      <div className={styles.productsWrapper}>
         {products.map((product) => {
           return <Product key={product.id} product={product} />;
         })}
