7bcedc0fbf95f1abcf1f6e04baf12bf7cdcee352
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-23 00:43:04 +0100

    feat: update Product component with styling and structure for improved presentation

--- front-end/src/components/Product/Product.jsx
+++ front-end/src/components/Product/Product.jsx
@@ -1,11 +1,13 @@
+import styles from "./Product.module.css";
 import { Link } from "react-router-dom";
 
 export function Product({ product }) {
   return (
-    <Link>
+    <Link className={styles.product}>
       <img src={product.photos[0]} />
       <h3>{product.productName}</h3>
       <p>{product.priceGBP}</p>
+      <div className={styles.heart}></div>
     </Link>
   );
 }
