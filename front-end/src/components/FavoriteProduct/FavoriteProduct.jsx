77535b3e751285d319f0b1f43786b219b862646a
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-24 01:50:01 +0100

    feat: add styling for FavoriteProduct component and improve layout

--- front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
+++ front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
@@ -1,22 +1,23 @@
+import styles from "./FavoriteProduct.module.css";
 import REMOVE_ICON from "../../assets/remove.svg";
 import BAG_ICON from "../../assets/bag.svg";
 
 export function FavoriteProduct({ product }) {
   return (
-    <div>
+    <div className={styles.favoriteProduct}>
       <img src={product.photos[0]} />
-      <div>
-        <div>
+      <div className={styles.favoriteProductInfo}>
+        <div className={styles.topRow}>
           <h3>
             {product.brand} {product.productName}
           </h3>
           <p>{product.priceGBP}£</p>
         </div>
-        <p>
+        <p className={styles.priceRow}>
           <span>Price: </span>
           {product.priceGBP}£
         </p>
-        <div>
+        <div className={styles.buttonRow}>
           <button>
             <img src={REMOVE_ICON} alt="Remove" />
             Remove
