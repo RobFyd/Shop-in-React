1da339d6aeee2772474c4c854767bafb252a61a5
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-24 01:24:54 +0100

    feat: enhance FavoriteProduct component with product details and action buttons

--- front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
+++ front-end/src/components/FavoriteProduct/FavoriteProduct.jsx
@@ -1,3 +1,32 @@
+import REMOVE_ICON from "../../assets/remove.svg";
+import BAG_ICON from "../../assets/bag.svg";
+
 export function FavoriteProduct({ product }) {
-  return <p>{product.productName}</p>;
+  return (
+    <div>
+      <img src={product.photos[0]} />
+      <div>
+        <div>
+          <h3>
+            {product.brand} {product.productName}
+          </h3>
+          <p>{product.priceGBP}£</p>
+        </div>
+        <p>
+          <span>Price: </span>
+          {product.priceGBP}£
+        </p>
+        <div>
+          <button>
+            <img src={REMOVE_ICON} alt="Remove" />
+            Remove
+          </button>
+          <button>
+            <img src={BAG_ICON} alt="Add to cart" />
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
