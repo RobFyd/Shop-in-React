63a1e153dafab540b0097531552aecd6c62eba84
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-24 01:10:29 +0100

    feat: implement FavoritesList component with styling

--- front-end/src/components/FavoritesList/FavoritesList.jsx
+++ front-end/src/components/FavoritesList/FavoritesList.jsx
@@ -1,14 +1,17 @@
+import styles from "./FavoritesList.module.css";
 import { CeneteredContent } from "../CenteredContent/CenteredContent";
 import { FavoriteProduct } from "../FavoriteProduct/FavoriteProduct";
 
 export function FavoritesList({ products }) {
   return (
     <CeneteredContent>
-      <h2>Favorite</h2>
-      <div>
-        {products.map((product) => {
-          return <FavoriteProduct key={product.id} product={product} />;
-        })}
+      <div className={styles.favoritesList}>
+        <h2>Favorite</h2>
+        <div>
+          {products.map((product) => {
+            return <FavoriteProduct key={product.id} product={product} />;
+          })}
+        </div>
       </div>
     </CeneteredContent>
   );
