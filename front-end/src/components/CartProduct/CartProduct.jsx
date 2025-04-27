89631aa167606cbbbcf2c3edaeb217798bdd32dd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-04-27 23:43:33 +0100

    feat: implement CartProduct component with styling

---
+++ front-end/src/components/CartProduct/CartProduct.jsx
@@ -0,0 +1,28 @@
+import styles from "./CartProduct.module.css";
+import REMOVE_ICON from "../../assets/remove.svg";
+
+export function CartProduct({ product }) {
+  return (
+    <div className={styles.favoriteProduct}>
+      <img src={product.photos[0]} />
+      <div className={styles.favoriteProductInfo}>
+        <div className={styles.topRow}>
+          <h3>
+            {product.brand} {product.productName}
+          </h3>
+          <p>{product.priceGBP}£</p>
+        </div>
+        <p className={styles.priceRow}>
+          <span>Price: </span>
+          {product.priceGBP}£
+        </p>
+        <div className={styles.buttonRow}>
+          <button>
+            <img src={REMOVE_ICON} alt="Remove" />
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
