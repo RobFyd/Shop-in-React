2868108d54d972364c6db6cc624b1d6bed73917d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-09 00:43:59 +0100

    feat: implement ExpandableMenu component and integrate it into ProductsList view

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -1,1 +1,20 @@
-export function ExpandableMenu() {}
+import { CATEGORIES } from "../../constants/categories";
+import { NavLink } from "react-router-dom";
+// import ARROW_ICON from "../../assets/icons/arrow.svg";
+
+export function ExpandableMenu() {
+  return (
+    <div>
+      <p>Women</p>
+      <ul>
+        {CATEGORIES.map((category) => {
+          return (
+            <li>
+              <NavLink>{category.categoryName}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
--- front-end/src/views/ProductsList/ProductsList.jsx
+++ front-end/src/views/ProductsList/ProductsList.jsx
@@ -1,10 +1,13 @@
 import { Layout } from "../../components/Layout/Layout.jsx";
 import { FlexContainer } from "../../components/FlexContainer/FlexContainer.jsx";
+import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu.jsx";
 
 export function ProductsList() {
   return (
     <Layout>
-      <FlexContainer></FlexContainer>
+      <FlexContainer>
+        <ExpandableMenu />
+      </FlexContainer>
     </Layout>
   );
 }
