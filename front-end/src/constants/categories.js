a260832d29fe6ef0d1ccdb59c430fe780dbaddfb
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-08 23:55:36 +0100

    fix: add subcategories for each main category in categories.js

--- front-end/src/constants/categories.js
+++ front-end/src/constants/categories.js
@@ -17,17 +17,69 @@ export const CATEGORIES = [
   {
     categoryName: "Clothes",
     path: "clothes",
+    subcategories: [
+      {
+        categoryName: "T-shirts",
+        path: "t-shirts",
+      },
+      {
+        categoryName: "Sweaters",
+        path: "sweaters",
+      },
+      {
+        categoryName: "Pants",
+        path: "pants",
+      },
+    ],
   },
   {
     categoryName: "Footwear",
     path: "footwear",
+    subcategories: [
+      {
+        categoryName: "Elegant",
+        path: "elegant",
+      },
+      {
+        categoryName: "Sports",
+        path: "sports",
+      },
+      {
+        categoryName: "Sneakers",
+        path: "sneakers",
+      },
+    ],
   },
   {
     categoryName: "Accessories",
     path: "accessories",
+    subcategories: [
+      {
+        categoryName: "Bags",
+        path: "bags",
+      },
+      {
+        categoryName: "Watches",
+        path: "watches",
+      },
+    ],
   },
   {
     categoryName: "Sport",
     path: "sport",
+    subcategories: [
+      {
+        categoryName: "Football",
+        path: "football",
+      },
+      {
+        categoryName: "Skis",
+        path: "skis",
+      },
+      {
+        categoryName: "Swimming",
+        path: "swimming",
+      },
+    ],
   },
 ];
