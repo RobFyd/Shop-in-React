cde6ea2e4bcbb8828f38eaf178d970708365a23b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-12 23:58:27 +0100

    feat: implement Breadcrumbs component with initial breadcrumb data

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,1 +1,19 @@
-export function Breadcrumbs() {}
+import { NavLink } from "react-router-dom";
+import ARROW_ICON from "../../assets/icons/arrow.svg";
+
+export function Breadcrumbs() {
+  const breadcrumbs = [
+    {
+      categoryName: "Women",
+      path: "women",
+    },
+    {
+      categoryName: "Clothes",
+      path: "clothes",
+    },
+    {
+      categoryName: "Sweaters",
+      parh: "sweaters",
+    },
+  ];
+}
