5109f1a4635dd3f8a82a1cbc488541464b6a6e3d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-19 00:49:35 +0100

    feat: add Pagination component for page navigation

---
+++ front-end/src/views/Pagination/Pagination.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+export function Pagination({ numberOfPages }) {
+  const pages = Array(numberOfPages).fill(null);
+  return (
+    <ul>
+      {pages.map((page, index) => {
+        return (
+          <li key={index}>
+            <NavLink>{index + 1}</NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
