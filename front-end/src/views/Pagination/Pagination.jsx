244cb47f366063e1061b1bdee9c1127f43a2074b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-19 01:18:08 +0100

    feat: implement Pagination component with styling for improved navigation

--- front-end/src/views/Pagination/Pagination.jsx
+++ front-end/src/views/Pagination/Pagination.jsx
@@ -1,9 +1,10 @@
+import styles from "./Pagination.module.css";
 import { NavLink } from "react-router-dom";
 
 export function Pagination({ numberOfPages }) {
   const pages = Array(numberOfPages).fill(null);
   return (
-    <ul>
+    <ul className={styles.pagination}>
       {pages.map((page, index) => {
         return (
           <li key={index}>
