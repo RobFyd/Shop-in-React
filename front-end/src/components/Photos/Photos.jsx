55ca696f8fa92084624e38ef0070e65223134e0b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-27 23:40:20 +0100

    feat: enhance photo selection functionality and update active thumbnail border style

--- front-end/src/components/Photos/Photos.jsx
+++ front-end/src/components/Photos/Photos.jsx
@@ -1,15 +1,25 @@
 import style from "./Photos.module.css";
 import { FlexContainer } from "../FlexContainer/FlexContainer";
+import { useState } from "react";
 
 export function Photos({ product }) {
+  const [currentPhoto, setCurrentPhoto] = useState(product.photos[0]);
+
   return (
     <FlexContainer>
       <div className={style.thumbnails}>
         {product.photos.map((photo) => {
-          return <img key={photo} src={photo} />;
+          return (
+            <img
+              className={currentPhoto === photo ? style.active : ""}
+              onClick={() => setCurrentPhoto(photo)}
+              key={photo}
+              src={photo}
+            />
+          );
         })}
       </div>
-      <img className={style.mainPhoto} src={product.photos[0]} />
+      <img className={style.mainPhoto} src={currentPhoto} />
     </FlexContainer>
   );
 }
