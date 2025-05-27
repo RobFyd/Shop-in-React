e3767409942d8ed5cbf54ad988a617be02f06be5
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-27 23:57:10 +0100

    feat: implement Details component with product information and delivery options

--- front-end/src/components/Details/Details.jsx
+++ front-end/src/components/Details/Details.jsx
@@ -1,3 +1,25 @@
-export function Details() {
-  return <div>Details</div>;
+import CAR_ICON from "../../assets/car.svg";
+import RETURN_ICON from "../../assets/return.svg";
+import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
+
+export function Details({ product }) {
+  return (
+    <div>
+      <h2>{product.brand}</h2>
+      <p>{product.productName}</p>
+      <p>{product.priceGBP} GBP</p>
+      <FullWidthButton isBlack={true} icon={CAR_ICON} text="Add to Cart" />
+      {/* <FullWidthButton icon={RETURN_ICON} text="Return Item" /> */}
+      <ul>
+        <li>
+          <img src={CAR_ICON} />
+          Delivery in 3-5 days
+        </li>
+        <li>
+          <img src={RETURN_ICON} />
+          Free returns within 30 days
+        </li>
+      </ul>
+    </div>
+  );
 }
