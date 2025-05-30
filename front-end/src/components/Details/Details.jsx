98c7b23ed400ab51be84fbc5f5d3cb6203d0ca2b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-30 23:31:21 +0100

    feat: add accordion functionality to Details component for enhanced product information display

--- front-end/src/components/Details/Details.jsx
+++ front-end/src/components/Details/Details.jsx
@@ -2,16 +2,30 @@ import styles from "./Details.module.css";
 import CAR_ICON from "../../assets/car.svg";
 import RETURN_ICON from "../../assets/return.svg";
 import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
+import { Accordion } from "../Accordion/Accordion";
 
 export function Details({ product }) {
+  const accordionContent = [
+    {
+      title: "Description",
+      content: product.description,
+    },
+    {
+      title: "Maintenance Info",
+      content: product.maintenanceInfo,
+    },
+  ];
+
   return (
     <div className={styles.details}>
       <h2>{product.brand}</h2>
       <p className={styles.productName}>{product.productName}</p>
       <p className={styles.price}>{product.priceGBP} GBP</p>
+
       <FullWidthButton isBlack={true} icon={CAR_ICON} text="Add to Cart">
         Add to Cart
       </FullWidthButton>
+
       <ul className={styles.extraInfo}>
         <li>
           <img src={CAR_ICON} />
@@ -22,6 +36,8 @@ export function Details({ product }) {
           Free returns within 30 days
         </li>
       </ul>
+
+      <Accordion items={accordionContent} />
     </div>
   );
 }
