d60996e4dcc4eee1f7bbe9b8dee4801425a01676
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-05-30 23:23:50 +0100

    feat: add Accordion component with basic structure and styling

---
+++ front-end/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,19 @@
+import ARROW_ICON from "../../assets/arrow.svg";
+
+export function Accordion({ items }) {
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            <div>
+              <p>{item.title}</p>
+              <img src={ARROW_ICON} />
+            </div>
+            <p>{item.content}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
