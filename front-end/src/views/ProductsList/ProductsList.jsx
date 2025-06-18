725bfecb6f81a7920d50fd622ae0c2d81d27d2e3
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-06-19 00:53:16 +0100

    feat: integrate useLoaderData and useParams for dynamic product fetching in ProductsList

--- front-end/src/views/ProductsList/ProductsList.jsx
+++ front-end/src/views/ProductsList/ProductsList.jsx
@@ -3,175 +3,179 @@ import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu.j
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs.jsx";
 import { Products } from "../../components/Products/Products.jsx";
 import { Pagination } from "../Pagination/Pagination.jsx";
+import { useLoaderData, useParams } from "react-router-dom";
 
 export function ProductsList() {
-  const products = [
-    {
-      id: 1,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-Shirt",
-      brand: "Top Brand",
-      pricePLN: 49,
-      priceUSD: 10,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (1).png",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 2,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-Shirt",
-      brand: "Top Brand",
-      pricePLN: 49,
-      priceUSD: 10,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (2).png",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 3,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-shirt 2",
-      brand: "Sun Tzu",
-      pricePLN: 199,
-      priceUSD: 49,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (3).png",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 4,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-shirt 3",
-      brand: "Sun Tzu",
-      pricePLN: 199,
-      priceUSD: 49,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (4).png",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 5,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-shirt 4",
-      brand: "Sun Tzu",
-      pricePLN: 129,
-      priceUSD: 39,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (1).png",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 6,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-Shirt",
-      brand: "Top Brand",
-      pricePLN: 49,
-      priceUSD: 10,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (2).png",
-        "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 7,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-shirt 2",
-      brand: "Sun Tzu",
-      pricePLN: 199,
-      priceUSD: 49,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (3).png",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 8,
-      gender: "men",
-      category: "odziez",
-      subcategory: "koszulki",
-      productName: "T-shirt 3",
-      brand: "Sun Tzu",
-      pricePLN: 199,
-      priceUSD: 49,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (4).png",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-  ];
+  // const products = [
+  //   {
+  //     id: 1,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-Shirt",
+  //     brand: "Top Brand",
+  //     pricePLN: 49,
+  //     priceUSD: 10,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (1).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 2,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-Shirt",
+  //     brand: "Top Brand",
+  //     pricePLN: 49,
+  //     priceUSD: 10,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (2).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 3,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-shirt 2",
+  //     brand: "Sun Tzu",
+  //     pricePLN: 199,
+  //     priceUSD: 49,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (3).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 4,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-shirt 3",
+  //     brand: "Sun Tzu",
+  //     pricePLN: 199,
+  //     priceUSD: 49,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (4).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 5,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-shirt 4",
+  //     brand: "Sun Tzu",
+  //     pricePLN: 129,
+  //     priceUSD: 39,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (1).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 6,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-Shirt",
+  //     brand: "Top Brand",
+  //     pricePLN: 49,
+  //     priceUSD: 10,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (2).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 7,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-shirt 2",
+  //     brand: "Sun Tzu",
+  //     pricePLN: 199,
+  //     priceUSD: 49,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (3).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  //   {
+  //     id: 8,
+  //     gender: "men",
+  //     category: "odziez",
+  //     subcategory: "koszulki",
+  //     productName: "T-shirt 3",
+  //     brand: "Sun Tzu",
+  //     pricePLN: 199,
+  //     priceUSD: 49,
+  //     photos: [
+  //       "http://localhost:3000/product-photos/wt1 (4).png",
+  //       "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
+  //       "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
+  //     ],
+  //     description:
+  //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
+  //     maintenanceInfo:
+  //       "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
+  //   },
+  // ];
+
+  const products = useLoaderData();
+  const params = useParams();
 
   return (
     <FlexContainer>
       <ExpandableMenu />
       <div>
         <Breadcrumbs />
-        <Products products={products} headerText="Sweaters" />
+        <Products headerText={params.category} products={products} />
         <Pagination numberOfPages={5} />
       </div>
     </FlexContainer>
