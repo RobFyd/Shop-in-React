986a3246cebfe58fbdbc170cfb420d30a5a5262f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-06-19 00:04:38 +0100

    feat: enhance productListLoader to fetch products by gender and category parameters

--- front-end/src/api/productListLoader.js
+++ front-end/src/api/productListLoader.js
@@ -1,5 +1,7 @@
-import { BACK_END_URL } from "../constants/api";
+import { BACK_END_URL, PATH_TO_ENDPOINT_MAPPING } from "../constants/api";
 
 export function productListLoader({ params: { gender, category } }) {
-  return fetch(`${BACK_END_URL}/products`);
+  return fetch(
+    `${BACK_END_URL}/products/?gender=${PATH_TO_ENDPOINT_MAPPING[gender]}&category=${category}`
+  );
 }
