f0d5e96c9c7587853e0d821da79770a7273f7f18
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-06-19 00:11:45 +0100

    refactor: validate category existence in productListLoader before fetching products

--- front-end/src/api/productListLoader.js
+++ front-end/src/api/productListLoader.js
@@ -1,7 +1,15 @@
+import { redirect } from "react-router-dom";
 import { BACK_END_URL, PATH_TO_ENDPOINT_MAPPING } from "../constants/api";
+import { CATEGORIES } from "../constants/categories";
 
 export function productListLoader({ params: { gender, category } }) {
-  return fetch(
-    `${BACK_END_URL}/products/?gender=${PATH_TO_ENDPOINT_MAPPING[gender]}&category=${category}`
-  );
+  const foundCategory = CATEGORIES.find((c) => c.path === category);
+
+  if (foundCategory) {
+    return fetch(
+      `${BACK_END_URL}/products/?gender=${PATH_TO_ENDPOINT_MAPPING[gender]}&category=${category}`
+    );
+  } else {
+    redirect("/main");
+  }
 }
