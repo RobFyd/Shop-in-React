d6079e69a49c1b0295c76eda5568d1695b95022f
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-01 00:39:00 +0100

    fix: add productLoader to ProductDetails route for data fetching

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -11,6 +11,7 @@ import { ProductDetails } from "./views/ProductDetails/ProductDetails.jsx";
 import { Layout } from "./components/Layout/Layout.jsx";
 import { mainPageLoader } from "./api/mainPageLoader.js";
 import { productListLoader } from "./api/productListLoader.js";
+import { productLoader } from "./api/productLoader.js";
 
 const router = createBrowserRouter([
   {
@@ -38,6 +39,7 @@ const router = createBrowserRouter([
       {
         path: "/:gender/:category/:subcategory/:productId",
         element: <ProductDetails />,
+        loader: productLoader,
       },
     ],
   },
