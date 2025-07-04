c150b9ae1dcd163fc47586d752d0ef4365e97f36
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-05 00:25:26 +0100

    fix: implement addProductToFavourites function to handle API requests

--- front-end/src/api/addProductToFavouritesAction.js
+++ front-end/src/api/addProductToFavouritesAction.js
@@ -1,1 +1,13 @@
-export function addProductToFavourites() {}
+import { BACK_END_URL } from "../constants/api";
+
+export function addProductToFavourites({ params: { productId } }) {
+  return fetch(`${BACK_END_URL}/favourites`, {
+    method: "POST",
+    body: JSON.stringify({
+      productId: Number(productId),
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
