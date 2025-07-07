7e6448503eddd1d98ac38b849acc9fd749651247
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-08 00:47:29 +0100

    refactor: remove FavoriteProduct, FavoritesList components and associated styles

--- front-end/src/views/Favorites/Favorites.jsx
+++
@@ -1,7 +0,0 @@
-import { useLoaderData } from "react-router-dom";
-import { FavoritesList } from "../../components/FavoritesList/FavoritesList";
-
-export function Favorites() {
-  const favoritesProducts = useLoaderData();
-  return <FavoritesList favourites={favoritesProducts} />;
-}
