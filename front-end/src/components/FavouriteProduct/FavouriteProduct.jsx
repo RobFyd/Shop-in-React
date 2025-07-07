f54967ecec391934863b2fd7a76eb3bf751d34e0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-08 00:49:35 +0100

    feat: add FavouriteProduct component for displaying favourite items

---
+++ front-end/src/components/FavouriteProduct/FavouriteProduct.jsx
@@ -0,0 +1,42 @@
+import styles from "./FavouriteProduct.module.css";
+import REMOVE_ICON from "../../assets/remove.svg";
+import BAG_ICON from "../../assets/bag.svg";
+import { useFetcher } from "react-router-dom";
+
+export function FavouriteProduct({ favourite }) {
+  const product = favourite.product;
+  const { Form } = useFetcher();
+
+  return (
+    <div className={styles.favouriteProduct}>
+      <img src={product.photos[0]} />
+      <div className={styles.favouriteProductInfo}>
+        <div className={styles.topRow}>
+          <h3>
+            {product.brand} {product.productName}
+          </h3>
+          <p>{product.priceGBP}£</p>
+        </div>
+        <p className={styles.priceRow}>
+          <span>Price: </span>
+          {product.priceGBP}£
+        </p>
+        <div className={styles.buttonRow}>
+          <Form
+            action={`/delete-from-favourites/${favourite.id}`}
+            method="DELETE"
+          >
+            <button>
+              <img src={REMOVE_ICON} alt="Remove" />
+              Remove
+            </button>
+          </Form>
+          <button>
+            <img src={BAG_ICON} alt="Add to cart" />
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
