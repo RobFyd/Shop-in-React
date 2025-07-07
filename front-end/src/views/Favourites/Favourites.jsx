6a4538df960291f634099b29ed3b1516fc489006
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-08 00:49:48 +0100

    feat: add Favourites view component to display a list of favourite products

---
+++ front-end/src/views/Favourites/Favourites.jsx
@@ -0,0 +1,7 @@
+import { useLoaderData } from "react-router-dom";
+import { FavouritesList } from "../../components/FavouritesList/FavouritesList";
+
+export function Favourites() {
+  const favouritesProducts = useLoaderData();
+  return <FavouritesList favourites={favouritesProducts} />;
+}
