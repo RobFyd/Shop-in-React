6cfff352e672a8c4b103c5f1d4b1a11f9aef358e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-10 23:35:02 +0100

    fix: correct variable name from 'favouritesProducts' to 'favouriteProducts' for consistency

--- front-end/src/views/Favourites/Favourites.jsx
+++ front-end/src/views/Favourites/Favourites.jsx
@@ -2,6 +2,6 @@ import { useLoaderData } from "react-router-dom";
 import { FavouritesList } from "../../components/FavouritesList/FavouritesList";
 
 export function Favourites() {
-  const favouritesProducts = useLoaderData();
-  return <FavouritesList favourites={favouritesProducts} />;
+  const favouriteProducts = useLoaderData();
+  return <FavouritesList favourites={favouriteProducts} />;
 }
