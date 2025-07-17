c601f7f2f11b3939c9b80f6284b5d9b508fe8571
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-07-17 23:56:04 +0100

    fix: correct typo in favouriteId parameter in deleteFavouriteAction

--- front-end/src/api/deleteFavouriteAction.js
+++ front-end/src/api/deleteFavouriteAction.js
@@ -1,7 +1,7 @@
 import { BACK_END_URL } from "../constants/api";
 
 export function deleteFavouriteAction({ params }) {
-  return fetch(`${BACK_END_URL}/favourites/${params.favourteId}`, {
+  return fetch(`${BACK_END_URL}/favourites/${params.favouriteId}`, {
     method: "DELETE",
   });
 }
