ebc0d35e2a1884bee4cc5cc3fb644da5a1d703dd
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-08-11 23:58:31 +0100

    feat: enhance CartSummary component to display prices with the correct currency sign

--- front-end/src/components/CartSummary/CartSummary.jsx
+++ front-end/src/components/CartSummary/CartSummary.jsx
@@ -3,7 +3,7 @@ import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
 import CAR_ICON from "../../assets/car.svg";
 import { useContext } from "react";
 import { CurrencyContext } from "../../contexts/CurrencyContext";
-import { CURRENCIES } from "../../constants/currencies";
+import { CURRENCIES, CURRENCY_SIGN } from "../../constants/currencies";
 
 export function CartSummary({ products }) {
   const [currency] = useContext(CurrencyContext);
@@ -17,6 +17,8 @@ export function CartSummary({ products }) {
     [CURRENCIES.GBP]: 100,
     [CURRENCIES.PLN]: 250,
   };
+
+  const currencySign = CURRENCY_SIGN[currency];
 
   const deliveryCost = deliveryCosts[currency];
   const minSumForFreeDelivery = minSumsForFreeDelivery[currency];
@@ -33,20 +35,32 @@ export function CartSummary({ products }) {
       <h2>Summary</h2>
       <div className={styles.cartRow}>
         <p>Value of products:</p>
-        <p>£{sum}</p>
+        <p>
+          {sum}
+          {currencySign}
+        </p>
       </div>
       <div className={styles.cartRow}>
         <p>Shipping cost:</p>
-        <p>£{sum > minSumForFreeDelivery ? 0 : deliveryCost}</p>
+        <p>
+          {sum > minSumForFreeDelivery ? 0 : deliveryCost}
+          {currencySign}
+        </p>
       </div>
       <div className={`${styles.cartRow} ${styles.cartSummaryRow}`}>
         <p>To be paid:</p>
-        <p>£{totalCost}</p>
+        <p>
+          {totalCost}
+          {currencySign}
+        </p>
       </div>
       <FullWidthButton isBlack={true}>Checkout</FullWidthButton>
       <div className={styles.deliveryInfo}>
         <img src={CAR_ICON} />
-        <p>Free delivery over £{minSumForFreeDelivery}</p>
+        <p>
+          Free delivery over {minSumForFreeDelivery}
+          {currencySign}
+        </p>
       </div>
     </div>
   );
