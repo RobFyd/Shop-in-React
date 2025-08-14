cf328ac86e226c06e31164aa1aae1a99ddabc5f3
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-08-14 23:57:46 +0100

    refactor: remove hardcoded cartProducts data from Cart component

--- front-end/src/views/Cart/Cart.jsx
+++ front-end/src/views/Cart/Cart.jsx
@@ -3,53 +3,6 @@ import { CartSummary } from "../../components/CartSummary/CartSummary";
 import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
 
 export function Cart() {
-  const cartProducts = [
-    {
-      id: 2,
-      gender: "men",
-      category: "clothes",
-      subcategory: "t-shirts",
-      productName: "T-Shirt",
-      // brand: "Top Brand",
-      brand: "Casualx",
-      priceGBP: 19.99,
-      pricePLN: 49,
-      priceUSD: 10,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (2).png",
-        "http://localhost:3000/product-photos/man-t-shirt-1.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-    {
-      id: 3,
-      gender: "men",
-      category: "clothes",
-      subcategory: "t-shirts",
-      productName: "T-shirt",
-      // productName: "T-shirt 2",
-      // brand: "Sun Tzu",
-      brand: "Casualx",
-      priceGBP: 19.99,
-      pricePLN: 199,
-      priceUSD: 49,
-      photos: [
-        "http://localhost:3000/product-photos/wt1 (3).png",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-3.jpg",
-        "http://localhost:3000/product-photos/man-t-shirt-4.jpg",
-      ],
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla facilis aperiam, magnam dolorum sit expedita nihil nostrum, voluptates temporibus voluptatum atque ullam molestiae provident dolore eligendi? Esse amet dolore illum.",
-      maintenanceInfo:
-        "Nemo et nam quasi in suscipit earum odit laborum repellat quo dolore rem, sequi eaque sapiente quibu",
-    },
-  ];
   return (
     <FlexContainer>
       <CartProductsList products={cartProducts} />
