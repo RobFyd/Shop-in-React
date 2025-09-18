4516b4b5e2cf2092c3a928afd21052d51795e0bf
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-09-18 23:47:11 +0100

    fix: update BACK_END_URL to use production endpoint

--- front-end/src/constants/api.js
+++ front-end/src/constants/api.js
@@ -6,4 +6,6 @@ export const PATH_TO_ENDPOINT_MAPPING = {
   kids: "children",
 };
 
-export const BACK_END_URL = "http://localhost:3000";
+// export const BACK_END_URL = "http://localhost:3000";
+
+export const BACK_END_URL = "https://shop-in-react.onrender.com";
