1ef3ecd035b1dd8e073cfa91302dbc764a8f9cb1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-11-29 00:27:36 +0000

    feat: add FullWidthButton story for component showcase

---
+++ front-end/src/stories/FullWidthButton.stories.js
@@ -0,0 +1,13 @@
+import { FullWidthButton } from "../components/FullWidthButton/FullWidthButton";
+
+export default {
+  component: FullWidthButton,
+  title: "FullWidthButton",
+};
+
+export const Default = {
+  args: {
+    isBlack: true,
+    children: "TEST black button",
+  },
+};
