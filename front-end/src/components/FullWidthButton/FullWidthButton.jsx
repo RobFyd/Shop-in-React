a9f27adea7eeab100f597a26bd1c01866f5df3b7
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-12-04 00:39:40 +0000

    feat: add isInverted prop to FullWidthButton for additional styling options

--- front-end/src/components/FullWidthButton/FullWidthButton.jsx
+++ front-end/src/components/FullWidthButton/FullWidthButton.jsx
@@ -1,9 +1,10 @@
 import styles from "./FullWidthButton.module.css";
 
-export function FullWidthButton({ children, onClick, isBlack }) {
+export function FullWidthButton({ children, onClick, isBlack, isInverted }) {
   return (
     <button
-      className={`${styles.button} ${isBlack ? styles.black : ""}`}
+      className={`${styles.button} ${isBlack ? styles.black : ""}
+      ${isInverted ? styles.inverted : ""}`}
       onClick={onClick}
     >
       {children}
