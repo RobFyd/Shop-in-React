1ffc899279ae7d92249a7554b167bc04c5d17d83
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-03-26 23:36:23 +0000

    fix: update test name for clarity in cart functionality test

--- tests/cartTest.spec.js
+++ tests/cartTest.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("test of adding a product to the cart", async ({ page }) => {
   await page.goto("http://localhost:5173/main");
   await page.getByRole("link", { name: "Clothes" }).click();
   await page.getByRole("link", { name: "Footwear arrow" }).click();
