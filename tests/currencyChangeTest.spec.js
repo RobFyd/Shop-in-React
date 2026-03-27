c92a6e6534f0a9254e55f5ca2eab536ee77c3133
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2026-03-27 00:20:01 +0000

    feat: implement currency change test with validation for PLN and USD

--- tests/currencyChangeTest.spec.js
+++ tests/currencyChangeTest.spec.js
@@ -1,3 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("test of changing the currency", async ({ page }) => {});
+test("test of changing the currency", async ({ page }) => {
+  await page.goto("http://localhost:5173/main");
+  const currencySelect = page.getByRole("combobox");
+
+  await expect(currencySelect).toHaveValue("PLN");
+  await expect(page.getByRole("link", { name: "Szpiliki 49zł" })).toBeVisible();
+
+  await currencySelect.selectOption("USD");
+
+  await expect(currencySelect).toHaveValue("USD");
+  await expect(page.getByRole("link", { name: "Szpiliki 10$" })).toBeVisible();
+
+  await page.reload();
+
+  await expect(currencySelect).toHaveValue("USD");
+  await expect(page.getByRole("link", { name: "Szpiliki 10$" })).toBeVisible();
+});
